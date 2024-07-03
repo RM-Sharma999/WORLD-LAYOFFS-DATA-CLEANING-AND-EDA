53439748ca0946f73fe080d429d06a2d4252e601
Rajan Madhav Sharma <madhavrajan1999@gmail.com>
2024-07-03 12:37:40 +0530

    Update World layoffs Data Cleaning and EDA.sql

--- World layoffs Data Cleaning and EDA.sql
+++ World layoffs Data Cleaning and EDA.sql
@@ -1,15 +1,15 @@
 #Data cleaning:
 
 #Creating a copy of the table layoffs_data:
-DROP TABLE IF EXISTS layoffs_cleaned;
+DROP TABLE IF EXISTS world_layoffs.layoffs_cleaned;
 
-CREATE TABLE layoffs_cleaned
-LIKE layoffs_data;
+CREATE TABLE world_layoffs.layoffs_cleaned
+LIKE world_layoffs.layoffs_data;
 
-INSERT INTO layoffs_cleaned
-SELECT * FROM layoffs_data;
+INSERT INTO world_layoffs.layoffs_cleaned
+SELECT * FROM world_layoffs.layoffs_data;
 
-SELECT * FROM layoffs_cleaned;
+SELECT * FROM world_layoffs.layoffs_cleaned;
 
 #1) Remove duplicates:
 -- [Using temp tables to handle duplicate rows!]
@@ -18,7 +18,7 @@ CREATE TEMPORARY TABLE duplicate_table(
   `Company` text,
   `Location_HQ` text,
   `Industry` text,
-  `Laid_Off_Count` text,
+  `Laid_Off_Count` INT,
   `Percentage` text,
   `Date` text,
   `Source` text,
@@ -26,7 +26,7 @@ CREATE TEMPORARY TABLE duplicate_table(
   `Stage` text,
   `Date_Added` text,
   `Country` text,
-  `List_of_Employees_Laid_Off` text,
+  `List_of_Employees_Laid_Off` INT,
   `row_num` INT
 );
 
@@ -43,63 +43,64 @@ WHERE row_num > 1;
 ALTER TABLE duplicate_table
 DROP COLUMN row_num;
 
-TRUNCATE TABLE layoffs_cleaned;
+TRUNCATE TABLE world_layoffs.layoffs_cleaned;
 
-INSERT INTO layoffs_cleaned
+INSERT INTO world_layoffs.layoffs_cleaned
 SELECT * FROM duplicate_table;
 
 #2) Standardize the Data:
 SELECT Company, TRIM(Company)
-FROM layoffs_cleaned;
+FROM world_layoffs.layoffs_cleaned;
 
-UPDATE layoffs_cleaned
+UPDATE world_layoffs.layoffs_cleaned
 SET Company = TRIM(Company);
 
 -- Checking for any spelling errors:
 SELECT DISTINCT Location_HQ
-FROM layoffs_cleaned
+FROM world_layoffs.layoffs_cleaned
 ORDER BY Location_HQ;
 
 SELECT DISTINCT Industry
-FROM layoffs_cleaned
+FROM world_layoffs.layoffs_cleaned
 ORDER BY Industry;
 
 SELECT DISTINCT Country
-FROM layoffs_cleaned
+FROM world_layoffs.layoffs_cleaned
 ORDER BY Country;
 
 -- Changing the Date column to Standard formart:
-UPDATE layoffs_cleaned
+UPDATE world_layoffs.layoffs_cleaned
 SET `Date` = STR_TO_DATE(`Date`,'%Y-%m-%dT%H:%i:%s.%fZ');
 
-ALTER TABLE layoffs_cleaned
+ALTER TABLE world_layoffs.layoffs_cleaned
 MODIFY COLUMN `Date` DATE;
 
 -- Changing the datatypes of columns:
-UPDATE layoffs_cleaned
+ALTER TABLE world_layoffs.layoffs_cleaned
+MODIFY COLUMN Laid_Off_Count INT;
+
+ALTER TABLE world_layoffs.layoffs_cleaned
+MODIFY COLUMN Funds_Raised INT;
+
+-- Replacing all blank values with Null Values in columns:
+UPDATE world_layoffs.layoffs_cleaned
 SET Laid_Off_Count = NULL
 WHERE Laid_Off_Count = '';
 
-ALTER TABLE layoffs_cleaned
-MODIFY COLUMN Laid_Off_Count INT;
-
-UPDATE layoffs_cleaned
+UPDATE world_layoffs.layoffs_cleaned
 SET Funds_Raised = NULL
 WHERE Funds_Raised = '';
 
-ALTER TABLE layoffs_cleaned
-MODIFY COLUMN Funds_Raised INT;
-
 #3) NULL values, Blank Values or any other misleading values:
 SELECT t1.Company, t1.Industry, t2.Industry
-FROM layoffs_cleaned t1
-JOIN layoffs_cleaned t2
+FROM world_layoffs.layoffs_cleaned t1
+JOIN world_layoffs.layoffs_cleaned t2
 	ON t1.Company = t2.Company
 WHERE t1.Industry = 'Other'
 AND t2.Industry != 'Other';
 
-UPDATE layoffs_cleaned AS t1
-JOIN layoffs_cleaned AS t2
+UPDATE world_layoffs.layoffs_cleaned AS t1
+JOIN world_layoffs.layoffs_cleaned AS t2
 	ON t1.Company = t2.Company
 SET t1.Industry = t2.Industry
 WHERE t1.Industry = 'Other'
@@ -107,102 +108,105 @@ AND t2.Industry != 'Other';
 
 
 SELECT *
-FROM layoffs_cleaned;
+FROM world_layoffs.layoffs_cleaned;
 
 -- SELECT t1.Company, t1.Location_HQ, t1.Industry, t1.Date, t1.Stage, t2.Stage
--- FROM layoffs_cleaned t1
--- JOIN layoffs_cleaned t2
+-- FROM world_layoffs.layoffs_cleaned t1
+-- JOIN world_layoffs.layoffs_cleaned t2
 -- 	ON t1.Company = t2.Company
 -- WHERE t1.Stage = 'Unknown'
 -- AND t2.Stage != 'Unknown';
 
 #4) Remove any unwanted row or columns:
 SELECT * 
-FROM layoffs_cleaned
-WHERE Laid_Off_Count = ''
-AND Percentage = '';
+FROM world_layoffs.layoffs_cleaned
+WHERE Laid_Off_Count IS NULL
+AND Percentage IS NULL;
 
 DELETE
-FROM layoffs_cleaned
-WHERE Laid_Off_Count = ''
-AND Percentage = '';
+FROM world_layoffs.layoffs_cleaned
+WHERE Laid_Off_Count IS NULL
+AND Percentage IS NULL;
 
-ALTER TABLE layoffs_cleaned
+ALTER TABLE world_layoffs.layoffs_cleaned
 DROP COLUMN Date_Added,
 DROP COLUMN List_of_Employees_Laid_Off;
+
+
+---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
 
 --  Exploratory Data Analysis:
 -- Date Range:
 SELECT MIN(`Date`), MAX(`Date`)
-FROM layoffs_cleaned;
+FROM world_layoffs.layoffs_cleaned;
 
 -- Max Layoffs:
 SELECT MAX(Laid_Off_Count)
-FROM layoffs_cleaned;
+FROM world_layoffs.layoffs_cleaned;
 
 -- Max and Min Percentage_Layoff:
 SELECT MAX(Percentage), MIN(Percentage) 
-FROM layoffs_cleaned;
+FROM world_layoffs.layoffs_cleaned;
 
 -- Layoff Count of Companies with 100% Layoffs:
 SELECT *
-FROM layoffs_cleaned
+FROM world_layoffs.layoffs_cleaned
 WHERE Percentage = 1
 ORDER BY Laid_Off_Count DESC;
 
 -- Funds Raised by Companies with 100% Layoffs:
 SELECT *
-FROM layoffs_cleaned
+FROM world_layoffs.layoffs_cleaned
 WHERE Percentage = 1
 ORDER BY Funds_Raised DESC;
 
 -- Total Layoffs per Company:
 SELECT Company, SUM(Laid_Off_Count) AS Layoff_Count_per_Company
-FROM layoffs_cleaned
+FROM world_layoffs.layoffs_cleaned
 GROUP BY Company
 ORDER BY 2 DESC;
 
 -- Layoff_Count_per_Company_HQ:
 SELECT Company, Location_HQ, SUM(Laid_Off_Count) AS Layoff_Count_per_Location_HQ
-FROM layoffs_cleaned
+FROM world_layoffs.layoffs_cleaned
 GROUP BY Company, Location_HQ
 ORDER BY 3 DESC;
 
 -- Layoff_Count_per_Industry:
 SELECT Industry, SUM(Laid_Off_Count) AS Layoff_Count_per_Industry
-FROM layoffs_cleaned
+FROM world_layoffs.layoffs_cleaned
 GROUP BY Industry
 ORDER BY 2 DESC;
 
 -- Layoff_Count_per_Location:
 SELECT Location_HQ, SUM(Laid_Off_Count) AS Layoff_Count_per_Location
-FROM layoffs_cleaned 
+FROM world_layoffs.layoffs_cleaned 
 GROUP BY Location_HQ 
 ORDER BY 2 DESC;
 
 -- Layoff_Count_per_Country:
 SELECT Country, SUM(Laid_Off_Count) AS Layoff_Count_per_Country
-FROM layoffs_cleaned
+FROM world_layoffs.layoffs_cleaned
 GROUP BY Country
 ORDER BY 2 DESC;
 
 -- Layoff_Count_per_Year:
 SELECT YEAR(`Date`), SUM(Laid_Off_Count) AS Layoff_Count_per_Year
-FROM layoffs_cleaned
+FROM world_layoffs.layoffs_cleaned
 GROUP BY YEAR(`Date`)
 ORDER BY 1 DESC;
 
 -- Layoff_Count_per_Stage:
 SELECT Stage, SUM(Laid_Off_Count) AS Layoff_Count_per_Stage
-FROM layoffs_cleaned
+FROM world_layoffs.layoffs_cleaned
 GROUP BY Stage
 ORDER BY 2 DESC;
 
 -- Rolling_Total by every month and year:
 WITH Rolling_Total AS (
 	SELECT SUBSTRING(`Date`, 1, 7) AS `MONTH`, SUM(Laid_Off_Count) AS Total_layoff
-	FROM layoffs_cleaned
+	FROM world_layoffs.layoffs_cleaned
 	GROUP BY `MONTH`
 	ORDER BY 1
 )
@@ -212,7 +216,7 @@ FROM Rolling_Total;
 -- Rolling_Total by every month per year:
 WITH Rolling_Total AS (
 	SELECT SUBSTRING(`Date`, 1, 7) AS `MONTH`, SUM(Laid_Off_Count) AS Total_layoff
-	FROM layoffs_cleaned
+	FROM world_layoffs.layoffs_cleaned
 	GROUP BY `MONTH`
 	ORDER BY 1
 )
@@ -222,7 +226,7 @@ FROM Rolling_Total;
 -- Ranking every Company with Total_Layoffs per Year:
 WITH Company_Year (Company, Years, Total_Layoffs) AS (
 	SELECT Company, YEAR(`Date`), SUM(Laid_Off_Count)
-	FROM layoffs_cleaned
+	FROM world_layoffs.layoffs_cleaned
 	GROUP BY Company, YEAR(`Date`)
 )
 SELECT *,
@@ -233,7 +237,7 @@ ORDER BY Ranking;
 -- Top Five Ranking Companies with Total_Layoffs per Year:
 WITH Company_Year (Company, Years, Total_Layoffs) AS (
 	SELECT Company, YEAR(`Date`), SUM(Laid_Off_Count)
-	FROM layoffs_cleaned
+	FROM world_layoffs.layoffs_cleaned
 	GROUP BY Company, YEAR(`Date`)
 ),
 Company_Year_Rank AS (
